63d31607ce85510c1d2b30bff4489b349092ba0a
ProjectAborted <190236413+ProjectAborted@users.noreply.github.com>
2026-04-24 16:57:40 -0700

    fix: project-architecture

--- obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d5314b8e484943c5ca79331d137d2145710c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd868b51a2928eff08724b59486557f46a72cd01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
